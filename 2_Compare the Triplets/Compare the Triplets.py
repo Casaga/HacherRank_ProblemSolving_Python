3ba0d614c8e9813cc8a39a95c3b810258a6689dc
Casaga <abraham253253@hotmail.es>
2020-11-19 15:06:11 -0600

    Compare the Triplets

---
+++ 2_Compare the Triplets/Compare the Triplets.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    
+    if(len(a)==len(b)):
+        PointsA=0
+        PointsB=0
+        for i in range(len(a)):
+            if a[i]!=b[i]:
+                if a[i]>b[i]:
+                    PointsA+=1
+                else:
+                    PointsB+=1
+        return [PointsA,PointsB]
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
